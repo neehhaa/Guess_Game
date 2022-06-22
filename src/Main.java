dec14ed8efa78660ff5ec019984a41224e70bc05
neehhaa <nehawasnik95@outlook.com>
2022-06-22 17:07:30 +0530

    GUESS GAME

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+        int ranNumber = (int) (Math.random()*100);
+        int userNumber = 0;
+
+        do{
+            System.out.println("Guess the number between 1-100 : ");
+            userNumber = sc.nextInt();
+            if (userNumber == ranNumber){
+                System.out.println("VOILA!!!!....YOU GUESSED IT CORRECT..!!! :)");
+                break;
+            }
+            else if (userNumber>ranNumber){
+                System.out.println("NUMBER TOO LARGE!!!! :( ");
+            }
+            else {
+                System.out.println("NUMBER TOO SMALL!!!! :( ");
+            }
+        }while(userNumber >= 0);
+        System.out.print("NUMBER WAS : ");
+        System.out.println(ranNumber);
+
+
+
     }
 }
